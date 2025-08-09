a4f578b62ecd2114b38ac552b619ce8f318c5297
itsmesaadali <itmesaad@gmail.com>
2025-08-09 21:26:14 +0500

    add the cors for frontend

--- BACKEND/src/app.js
+++ BACKEND/src/app.js
@@ -1,12 +1,21 @@
 import express from 'express'
+import cors from 'cors'
 import shortUrlRoute from './routes/shorturl.route.js'
+import { redirectShortUrl } from './controllers/shorturl.controller.js'
 
 const app = express()
+
+app.use(cors({
+    origin:"http://localhost:5173",
+    credentials:true
+}))
 
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 
 app.use('/api/v1', shortUrlRoute)
+
+app.get('/:id',redirectShortUrl)
 
 
 
--- BACKEND/src/routes/shorturl.route.js
+++ BACKEND/src/routes/shorturl.route.js
@@ -1,9 +1,9 @@
 import {Router} from 'express'
-import { createShortUrl, redirectShortUrl } from '../controllers/shorturl.controller.js';
+import { createShortUrl } from '../controllers/shorturl.controller.js';
 
 const router = Router()
 
 router.route('/create').post(createShortUrl)
-router.route('/:id').get(redirectShortUrl)
+
 
 export default router;
